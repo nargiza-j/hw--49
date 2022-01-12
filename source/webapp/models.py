2ae7af59e73af4351b3082002cad411557dcbca1
nargiza-jumagulova <impression1108@gmail.com>
2022-01-13 02:18:06 +0600

    создали модели Task, Status, Type и применили миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Статус')
+
+    class Meta:
+        db_table = 'status'
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=300, verbose_name='Тип')
+
+    class Meta:
+        db_table = 'type'
+        verbose_name = 'Тип'
+        verbose_name_plural = 'Типы'
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=255, null=False, blank=False, verbose_name='Заголовок')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    status = models.ForeignKey('webapp.Status', related_name='status', on_delete=models.PROTECT, verbose_name='Статус')
+    type = models.ForeignKey('webapp.Type', related_name='type', on_delete=models.PROTECT, verbose_name='Тип')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    def __str__(self):
+        return f"{self.pk}.{self.summary}"
+
+    class Meta:
+        db_table = 'tasks'
+        verbose_name = 'задача'
+        verbose_name_plural = 'задачи'
