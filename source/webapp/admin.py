389e9601bb4489d64daeffa0cffbc2a5364629a4
nargiza-jumagulova <impression1108@gmail.com>
2022-01-13 02:37:36 +0600

    зарегистрировали модели в админ-панели и создали админа

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Task, Status, Type
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status', 'type']
+    list_filter = ['status', 'type']
+    search_fields = ['summary', 'type', 'status']
+    fields = ['summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
