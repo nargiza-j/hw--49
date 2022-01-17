8d9a3875a35dcd2f266592aaa5f38e91097de774
nargiza-jumagulova <impression1108@gmail.com>
2022-01-17 16:24:59 +0600

    добавили chechbox widget

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,9 +1,11 @@
 from django import forms
+from django.forms import widgets
 
-from webapp.models import Task
+from webapp.models import Task, Type
 
 
 class TaskForm(forms.ModelForm):
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label="Тип", widget=forms.CheckboxSelectMultiple)
 
     class Meta:
         model = Task
