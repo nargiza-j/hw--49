f9b178a0d74a87b20c44ff0c01348c4aac797083
nargiza-jumagulova <impression1108@gmail.com>
2022-01-20 17:40:51 +0600

    Добавили удобный функционал для связи многие ко многим в админку

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -3,14 +3,24 @@ from django.contrib import admin
 from webapp.models import Task, Status, Type
 
 
+class TaskInline(admin.TabularInline):
+    model = Task.type.through
+
+
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['id', 'summary', 'status']
     list_filter = ['status', 'type']
     search_fields = ['summary', 'type', 'status']
-    fields = ['summary', 'description', 'status', 'type', 'created_at', 'updated_at']
+    fields = ['summary', 'description', 'status', 'created_at', 'updated_at']
     readonly_fields = ['created_at', 'updated_at']
+    inlines = [TaskInline, ]
+    exclude = ('type',)
+
+
+class TypeAdmin(admin.ModelAdmin):
+    inlines = [TaskInline]
 
 
 admin.site.register(Task, TaskAdmin)
 admin.site.register(Status)
-admin.site.register(Type)
+admin.site.register(Type, TypeAdmin)
