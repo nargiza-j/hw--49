79feacb99d63378d21b3b90650a9a15f7b4d3a05
nargiza-jumagulova <impression1108@gmail.com>
2022-01-26 23:37:00 +0600

    добавлена пагинация

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -12,10 +12,15 @@ class IndexView(ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'index.html'
+    paginate_by = 10
 
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.order_by('-created_at')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        return context
 
 
 class TaskView(TemplateView):
