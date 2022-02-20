76cd0c2c7d6dec5374c7338d70f637854d08f379
nargiza-jumagulova <impression1108@gmail.com>
2022-02-21 02:17:23 +0600

    Создана модель Profile, которая имеет связь 1-к-1 с моделью пользователя, создана миграция. Модель добавлена в админ панель

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
+from .models import Profile
+
+User = get_user_model()
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    fields = ('avatar', 'profile_url', 'about_info')
+
+
+class UserProfileAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserProfileAdmin)
--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), related_name="profile", verbose_name="профиль",
+                                on_delete=models.CASCADE)
+    avatar = models.ImageField(null=True, blank=True, verbose_name="Аватар", upload_to="avatars/")
+    profile_url = models.URLField(max_length=200, null=True, blank=True, verbose_name="Профиль на GitHub")
+    about_info = models.TextField(max_length=300, null=True, blank=True, verbose_name="О себе")
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
+
