14c51e2ccd0099839ae56e7115c88d986088dd12
nargiza-jumagulova <impression1108@gmail.com>
2022-02-21 03:42:07 +0600

    Изменили Имена пользователей на страницах сайта ссылками на их личные страницы.

--- source/webapp/views/users.py
+++ source/webapp/views/users.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 from django.urls import reverse
 from django.views.generic import CreateView
 
@@ -6,9 +7,10 @@ from webapp.forms import UsersChooseForm
 
 
 class UsersChooseView(CreateView):
-    model = User
+    model = get_user_model()
     template_name = 'users/choose_user.html'
     form_class = UsersChooseForm
+    context_object_name = "user_obj"
 
     def get_success_url(self):
         return reverse('webapp:project_view', kwargs={'pk': self.object.pk})
