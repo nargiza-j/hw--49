2ab6e0607bd1b1174662e07220e24c54c7062ede
nargiza-jumagulova <impression1108@gmail.com>
2022-02-24 00:58:09 +0600

    написали представление UpdateUserView для редактирования профиля пользователя с ограничением доступа только для текущего пользователя

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,19 +1,20 @@
 from django.contrib.auth import authenticate, login, logout, get_user_model
-from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 
-
-# Create your views here.
 from django.urls import reverse
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView
 
-from accounts.forms import MyUserCreationForm
+from accounts.forms import MyUserCreationForm, UserUpdateForm, ProfileUpdateForm
 from accounts.models import Profile
+
+User = get_user_model()
 
 
 class RegisterView(CreateView):
-    model = get_user_model()
+    model = User
     template_name = 'registration/registration.html'
     form_class = MyUserCreationForm
 
@@ -65,7 +66,7 @@ def logout_view(request):
 
 
 class UserProfileView(DetailView):
-    model = get_user_model()
+    model = User
     template_name = 'profile.html'
     context_object_name = "user_object"
     paginate_related_by = 3
@@ -81,4 +82,44 @@ class UserProfileView(DetailView):
         return super().get_context_data(**kwargs)
 
 
+class UpdateUserView(LoginRequiredMixin, UpdateView):
+    model = User
+    form_class = UserUpdateForm
+    form_profile_class = ProfileUpdateForm
+    template_name = "update_profile.html"
+    context_object_name = "user_object"
 
+    def get_success_url(self):
+        return reverse("accounts:user-profile", kwargs={"pk": self.request.user.pk})
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        profile_form = self.get_profile_form()
+        if form.is_valid() and profile_form.is_valid():
+            return self.form_valid(form, profile_form)
+        else:
+            return self.form_invalid(form, profile_form)
+
+    def form_valid(self, form, profile_form):
+        profile_form.save()
+        return super().form_valid(form)
+
+    def form_invalid(self, form, profile_form):
+        context = self.get_context_data(form=form, profile_form=profile_form)
+        return self.render_to_response(context)
+
+    def get_profile_form(self):
+        form_kwargs = {'instance': self.object.profile}
+        if self.request.method == 'POST':
+            form_kwargs['data'] = self.request.POST
+            form_kwargs['files'] = self.request.FILES
+        return ProfileUpdateForm(**form_kwargs)
+
+    def get_context_data(self, **kwargs):
+        if 'profile_form' not in kwargs:
+            kwargs['profile_form'] = self.get_profile_form()
+        return super().get_context_data(**kwargs)
